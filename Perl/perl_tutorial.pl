7106e9c5056c30e1d8bc88baf7582b6c7fb14985
JMS 53 <jms53@x230.jms>
2020-01-19 00:23:25 +0100

    added perl tutorial

---
+++ Perl/perl_tutorial.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+
+#Chapter 1.1: Syntax
+#print "hello world\n"
+
+#Chapter 1.2: Data Types
+#my %foo = (a => 1, "b" => 2, 'c' => 'batman');
+#print $foo{c}; # prints 'batman'
+
